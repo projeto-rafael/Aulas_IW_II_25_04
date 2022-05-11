a048ae7e89bc945b6aa05d43f500d14195d41303
projeto-rafael <80163684+projeto-rafael@users.noreply.github.com>
2022-05-11 16:22:20 -0300

    Add files via upload

---
+++ IW II - Condicionais/exercicio 01/exercicio_01.js
@@ -0,0 +1,11 @@
+var letter = prompt("Digite uma letra");
+var letra = letter.toUpperCase();
+
+
+if ((letra == "A")  || (letra == "E") || (letra == "I") || (letra == "O") || (letra == "U")){
+    console.log("A letra digitada é uma vogal!");
+}
+ 
+else{
+    console.log("A letra é uma consoante!")
+}
